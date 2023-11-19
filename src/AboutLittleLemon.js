4ca9b766d4b91d8a2fd54d4cca65844ba614e6cb
Mahmoud Alwakeel <alwakeel@Mahmouds-MacBook-Pro.local>
2023-11-19 19:20:38 +0200

    video updates

--- src/AboutLittleLemon.js
+++ src/AboutLittleLemon.js
@@ -1,9 +1,14 @@
 import ReactPlayer from "react-player/youtube";
 function AboutLittleLemon() {
+    const vidURL ='https://youtube.com/shorts/ddb3J_Tk1Tg'
     return (
         <div>
             <h1>About Weka's Books</h1>
-            <ReactPlayer url='https://youtube.com/shorts/ddb3J_Tk1Tg'></ReactPlayer>
+            <ReactPlayer 
+            url={vidURL}
+            playing={false}
+            volume={0.8}
+            ></ReactPlayer>
         </div>
     )
 }
